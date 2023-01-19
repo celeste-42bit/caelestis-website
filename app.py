34da2c801c040ba7836b6f8c50652a9bfbba4813
celeste-42bit <studi.jasha.gruener@gmail.com>
2023-01-19 15:29:41 +0100

    SOF

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
+# ------------------------------------------
+# caelestis-website
+# app.py V.: 0.0.1
+# https://github.com/celeste-42bit/caelestis-website
+# Copyright (C) 2023 celeste-42bit : MIT
+# ------------------------------------------
+
 print("Hello World!")
